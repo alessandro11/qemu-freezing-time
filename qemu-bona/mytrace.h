8c279129e737a3c216b60d58a1dda96d3ac75f3f
Alessandro Elias <ae11@c3sl.ufpr.br>
2015-04-06 16:55:27 -0300

    Added file and line number in trace.
    Signed-off-by: Alessandro Elias <ae11@c3sl.ufpr.br>

--- qemu-bona/mytrace.h
+++ qemu-bona/mytrace.h
@@ -4,9 +4,12 @@
 #include <sys/syscall.h>
 
 #define  AETRACE1(msg, p1) \
-    fprintf(stderr, "\nAETRACE ThreadId: %ld " msg "\n", syscall(SYS_gettid), p1)
-
+    fprintf(stderr, "\nAETRACE %s(%d): ThreadId: %ld \n" msg "\n", __FILE__, __LINE__, syscall(SYS_gettid), (p1))
+#define  AETRACE2(msg, p1, p2) \
+    fprintf(stderr, "\nAETRACE %s(%d): ThreadId: %ld \n" msg "\n", __FILE__, __LINE__, syscall(SYS_gettid), (p1), (p2))
+#define  AETRACE3(msg, p1, p2, p3) \
+    fprintf(stderr, "\nAETRACE %s(%d): ThreadId: %ld \n" msg "\n", __FILE__, __LINE__, syscall(SYS_gettid), (p1), (p2), (p3))
 #define  AETRACE(msg) \
-    fprintf(stderr, "\nAETRACE ThreadId: %ld " msg "\n", syscall(SYS_gettid))
+    fprintf(stderr, "\nAETRACE %s(%d): ThreadId: %ld \n" msg "\n", __FILE__, __LINE__, syscall(SYS_gettid))
 
 #endif /* __AETRACE_H__ */
